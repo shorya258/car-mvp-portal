e866f260d882fda741d44d19942b52ce360b4f47
Shorya <shoryahayaran@gmail.com>
2024-11-15 16:35:51 +0530

    signup route

---
+++ src/app/api/signUp/route.js
@@ -0,0 +1,63 @@
+import dbConnect from "@/lib/dbConnect";
+import { NextResponse } from "next/server";
+import UserModel from "@/lib/model/User";
+const bcrypt = require("bcryptjs");
+export async function POST(req) {
+  await dbConnect();
+  try {
+    const body = await req.json();
+    const { username, email, password } = body;
+    if (!username || !email || !password) {
+      return NextResponse.json(
+        {
+          success: false,
+          message: "Missing required fields",
+        },
+        { status: 400 }
+      );
+    }
+    const existingUserByEmail = await UserModel.findOne({ email });
+    if (existingUserByEmail) {
+      return NextResponse.json(
+        {
+          success: false,
+          message: "User already exists",
+        },
+        { status: 400 }
+      );
+    }
+    const existingUserByUsername = await UserModel.findOne({ username });
+    if (existingUserByUsername) {
+      return NextResponse.json(
+        {
+          success: false,
+          message: "Username is already taken",
+        },
+        { status: 400 }
+      );
+    }
+    const salt = await bcrypt.genSalt(10);
+    let hashedPassword = await bcrypt.hash(password, salt);
+    const newUser = new UserModel({
+      username,
+      email,
+      password: hashedPassword,
+    });
+    await newUser.save();
+    return NextResponse.json(
+      {
+        success: true,
+        message: "User registered successfully!",
+      },
+      { status: 201 }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      {
+        success: false,
+        message: "Error registering user",
+      },
+      { status: 500 }
+    );
+  }
+}
