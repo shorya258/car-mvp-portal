2a7e61bee579a44d76a0b2cdf919a0a1aa46861e
Shorya <shoryahayaran@gmail.com>
2024-11-19 15:30:21 +0530

    dashboard ui for when no cars

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -3,7 +3,7 @@ import Navbar from "../Components/Navbar";
 import React, { useEffect, useState } from "react";
 import ProductCard from "../Components/ProductCard";
 import { jwtDecode } from "jwt-decode";
-import { faCarSide } from "@fortawesome/free-solid-svg-icons";
+import { faAdd, faCarSide, faCirclePlus } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const dashboard = () => {
@@ -19,9 +19,9 @@ const dashboard = () => {
       }),
     });
     const json = await response.json();
-    setFetchedProducts(json.products);
     if (response.status === 201) {
       console.log(json.message);
+      setFetchedProducts(json.products);
     } else {
       console.log(json.message);
     }
@@ -46,17 +46,25 @@ const dashboard = () => {
 
       {/* Bottom Section */}
       <div className="bg-gray-300 min-h-[60%] h-auto flex justify-center items-center ">
-        <div className={`p-8 bg-transparent min-h-full w-[90%] rounded-lg grid grid-cols-4 gap-8`}>
-          {fetchedProducts && fetchedProducts?.map((singleProductData, key) => (
-            <ProductCard singleProductData={singleProductData} key={key} />
-          ))}
+        <div
+          className={`p-8 bg-transparent min-h-full w-[90%] rounded-lg  ${
+            fetchedProducts.length!=0 ? "grid grid-cols-4 gap-8" : "flex "
+          } `}
+        >
+          {(fetchedProducts && fetchedProducts.length!=0) ? (
+            fetchedProducts?.map((singleProductData, key) => (
+              <ProductCard singleProductData={singleProductData} key={key} />
+            ))
+          ) : (
+            <div className="text-gray-500 w-full flex flex-col gap-2 bg-white rounded-md justify-center items-center border-dashed border-gray-600 border-2 hover:scale-105 transition-transform duration-300 ease-in-out cursor-default">
+              <FontAwesomeIcon icon={faCirclePlus } className="text-3xl" />
+              <span className="text-xl font-medium tracking-wider">
+                Click to add a car.
+              </span>
+            </div>
+          )}
         </div>
       </div>
-          {!fetchedProducts && <div className='text-gray-500  w-full '>
-            <FontAwesomeIcon icon={faCarSide} className="text-7xl" />
-            <span className="text-6xl font-extrabold tracking-wider" >SHOOT! YOU HAVE NOT ADDED ANY CARS YET!</span>
-          </div>
-            }
     </div>
   );
 };
