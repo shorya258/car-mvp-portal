4ad68b68cc388db142fceae0accd4aed1cb3ab84
Shorya <shoryahayaran@gmail.com>
2024-11-21 18:39:06 +0530

    refactor: ui for zero searched results

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -8,14 +8,14 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useRouter } from "next/navigation";
 import ProductContext from "@/context/ProductContext";
 import { ToastContainer, toast } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css';
+import "react-toastify/dist/ReactToastify.css";
 const dashboard = () => {
   const router = useRouter();
   const { setFetchedProducts } = useContext(ProductContext);
   const { fetchedProducts } = useContext(ProductContext);
   const [searchedTerm, setSearchedTerm] = useState("");
   const [filteredProducts, setFilteredProducts] = useState([]);
-  const[userId, setUserId]= useState("")
+  const [userId, setUserId] = useState("");
   const fetchAllProductsByUserID = async (userId) => {
     const response = await fetch("api/fetchAllProductsForUser", {
       method: "POST",
@@ -30,18 +30,16 @@ const dashboard = () => {
     if (response.status === 201) {
       setFetchedProducts(json.products);
       setFilteredProducts(json.products);
-    } 
-    else if(response.status === 200){
-      setFetchedProducts([])
-      setFilteredProducts([])
-    }
-    else {
+    } else if (response.status === 200) {
+      setFetchedProducts([]);
+      setFilteredProducts([]);
+    } else {
       console.log(json.message);
     }
   };
   const removeProduct = () => {
-    fetchAllProductsByUserID(userId)
-    toast.error("Removed successfully")
+    fetchAllProductsByUserID(userId);
+    toast.error("Removed successfully");
   };
   const handleSearchBar = (value) => {
     console.log("search bar clicked");
@@ -54,8 +52,9 @@ const dashboard = () => {
       const searchedProducts = fetchedProducts.filter((singleProduct) => {
         return singleProduct.productName
           .toLowerCase()
-          .includes(searchedTerm.toLowerCase())
+          .includes(searchedTerm.toLowerCase());
       });
+      console.log(searchedProducts);
       setFilteredProducts(searchedProducts);
     } else {
       setFilteredProducts(fetchedProducts);
@@ -66,14 +65,14 @@ const dashboard = () => {
       const authToken = localStorage.getItem("authStorageToken");
       if (authToken) {
         const storedData = jwtDecode(authToken);
-        setUserId(storedData.userId)
+        setUserId(storedData.userId);
         fetchAllProductsByUserID(storedData.userId);
       }
     }
   }, []);
   return (
     <div className="flex flex-col min-h-screen h-auto w-full max-w-screen mx-auto overflow-y-auto">
-        <ToastContainer/>
+      <ToastContainer />
       {/* Top Section */}
       <div className="bg-customBG h-[300px] bg-cover bg-no-repeat bg-bottom flex-shrink-0">
         <Navbar />
@@ -106,6 +105,8 @@ const dashboard = () => {
                 onDelete={removeProduct}
               />
             ))
+          ) : searchedTerm.length !== 0 ? (
+            <div className="text-gray-500 w-full h-[400px] flex rounded-md justify-center items-center cursor-default" >No products match the searched string!</div>
           ) : (
             <div
               className="text-gray-500 w-full h-[500px] flex flex-col gap-2 bg-white rounded-md justify-center items-center border-dashed border-gray-600 border-2 hover:scale-105 transition-transform duration-300 ease-in-out cursor-default"
